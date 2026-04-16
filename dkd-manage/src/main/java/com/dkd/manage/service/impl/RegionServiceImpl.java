56d2d8fd3e07260d219aa41420a8800a23a89a83
nanchao <2635469649@qq.com>
2026-04-16 15:43:02 +0800

    20260416 人员管理，人员列表

--- dkd-manage/src/main/java/com/dkd/manage/service/impl/RegionServiceImpl.java
+++ dkd-manage/src/main/java/com/dkd/manage/service/impl/RegionServiceImpl.java
@@ -2,12 +2,15 @@ package com.dkd.manage.service.impl;
 
 import java.util.List;
 import com.dkd.common.utils.DateUtils;
+import com.dkd.framework.interceptor.RepeatSubmitInterceptor;
 import com.dkd.manage.domain.vo.RegionVo;
+import com.dkd.manage.mapper.EmpMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.dkd.manage.mapper.RegionMapper;
 import com.dkd.manage.domain.Region;
 import com.dkd.manage.service.IRegionService;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 区域管理Service业务层处理
@@ -20,6 +23,10 @@ public class RegionServiceImpl implements IRegionService
 {
     @Autowired
     private RegionMapper regionMapper;
+    @Autowired
+    private EmpMapper empMapper;
+    @Autowired
+    private RepeatSubmitInterceptor repeatSubmitInterceptor;
 
     /**
      * 查询区域管理
@@ -64,11 +71,15 @@ public class RegionServiceImpl implements IRegionService
      * @param region 区域管理
      * @return 结果
      */
+    @Transactional(rollbackFor = Exception.class)
     @Override
     public int updateRegion(Region region)
     {
         region.setUpdateTime(DateUtils.getNowDate());
-        return regionMapper.updateRegion(region);
+        int result=regionMapper.updateRegion(region);
+        //同步更新员工表区域名称
+        empMapper.updateByRegionId(region.getRegionName(),region.getId());
+        return result;
     }
 
     /**
